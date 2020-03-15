1788edb256774de2ac701065339d1f9e4cd5b22d
Seungyeol <marudev4905@gmail.com>
2020-03-15 21:21:44 +0900

    파일 구조 변경 및 위도 경도 데이터 추출 코드 추가

---
+++ js/weather.js
@@ -0,0 +1,37 @@
+const COORDS = 'coords';
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location")
+}
+
+function askForCords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null){
+        askForCords();
+    }else{
+        //getWeather
+    }
+}
+
+function init(){
+    loadCoords();
+}
+init();
